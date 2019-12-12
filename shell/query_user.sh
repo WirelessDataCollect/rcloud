c72c2b54ada82ec52b5727d1f3c947a9e574c84b
Neyzoter <sonechaochao@gmail.com>
2019-12-12 22:27:33 +0800

    mongodb sh admin

--- shell/query_user.sh
+++ shell/query_user.sh
@@ -2,4 +2,5 @@
 # 查询所有用户
 
 cmd="db.admin.find({},{'userName':1,'userKey':1,'_id':0}).pretty();"
-echo $cmd | mongo data --shell
+# data : 需要访问的数据库；admin : 管理员保存的数据库
+echo $cmd | mongo data -u songchaochao -p songchaochao --authenticationDatabase admin --shell
