2dfc8b5ea0d58681e68ff137109f32c4b739d111
Neyzoter <sonechaochao@gmail.com>
2019-12-12 22:25:54 +0800

    mongodb sh admin

--- shell/freespace2mgd.sh
+++ shell/freespace2mgd.sh
@@ -80,6 +80,7 @@ fi
 cmd_left="db.space.update({},{\$set:{'free_space':'"
 cmd_right="'}},{upsert:true});"
 cmd="$cmd_left$path_avail$cmd_right"
-echo $cmd | mongo data --shell
+# data : 需要访问的数据库；admin : 管理员保存的数据库
+echo $cmd | mongo data -u songchaochao -p songchaochao --authenticationDatabase  admin --shell
 
 echo "run $cmd"
