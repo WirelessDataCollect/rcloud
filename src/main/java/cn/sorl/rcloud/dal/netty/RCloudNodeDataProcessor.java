ea70423add6f4c1b26ba1580a90b2050ac527448
Neyzoter <sonechaochao@gmail.com>
2020-04-13 21:10:46 +0800

    add Rcloud Node Data Processor

--- src/main/java/cn/sorl/rcloud/dal/netty/RCloudNodeDataProcessor.java
+++ src/main/java/cn/sorl/rcloud/dal/netty/RCloudNodeDataProcessor.java
@@ -19,62 +19,60 @@ import org.slf4j.LoggerFactory;
  * @apiNote  数据格式：[0:PACKAGE_TIME_IO_LENGTH-1]：[时间:ADC数据长度:IO:ID:校验码]；
  * [PACKAGE_TIME_IO_LENGTH-1:PACKAGE_TIME_IO_LENGTH+MAX_TEST_NAME-1]：[时间:ADC数据长度:IO:ID:校验码]；
  * 00 00 10 10 48 10 01 00 10 00 00 00 01 01 00 48 74 65 73 74 31 5f 32 30 31 39 30 33 30 31 5f 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01 02 00 05 01 03 00 06 01 02 00 05 01 03 00 05
- * @author  nesc420
- * @date    2019-4-8
- * @version 0.0.3
+ * @author  Charles Song
+ * @date    2020-4-13
+ * @version 0.0.1
  */
-public class RuiliNodeUdpDataProcessor {
+public class RCloudNodeDataProcessor {
     private int BytebufLength;
     private short checkUbyte;
     private short nodeId;
     private long yyyy_mm_dd;
     private long headtime;
-    //从testName获取的iso时间
+    /**
+     * 从testName获取的iso时间
+     */
     private String isodate;
-    //插入文档时候的iso时间
+    /**
+     * 插入文档时候的iso时间
+     */
     private String insertIsodate;
     private long data_count;
     private short io1,io2;
-    //数据类型 @ref “CAN“ or “ADC“
+    /**
+     * 数据类型 @ref “CAN“ or “ADC“
+     */
     private String dataType;
     private String testName;
 
     private SimpleMgd dataMgd;
-    private static final Logger logger = LoggerFactory.getLogger(RuiliNodeUdpDataProcessor.class);
+    private static final Logger logger = LoggerFactory.getLogger(RCloudNodeDataProcessor.class);
 
-    /**
-     * 构造类
-     */
-    public RuiliNodeUdpDataProcessor(SimpleMgd dataMgd){
+    public RCloudNodeDataProcessor (SimpleMgd dataMgd) {
         this.dataMgd = dataMgd;
     }
-//    @PostConstruct
-//    public void init(){
-//
-//    }
+
     /**
-     * 数据库连接对象的构造函数
-     *
-     * @param simpleMgd 数据库
-     * @return 无
+     * 设置MongoDB的操作对象
+     * @param simpleMgd mongodb操作对象
      */
     public void setMongodb(SimpleMgd simpleMgd) {
         this.dataMgd = simpleMgd;
         //指向当前的数据集合
         this.dataMgd.resetCol(TimeUtils.getStrIsoMTime());
     }
+
     /**
-     * 数据库连接对象返回
-     * @return
+     * 获取MongoDB的操作对象
+     * @return {@link SimpleMgd} mongodb操作对象
      */
     public SimpleMgd getMongodb() {
         return this.dataMgd;
     }
+
     /**
-     * 总的数据包的解析和存储方法
-     *
-     * @param msg 传入的ByteBuf数据。
-     * @return none
+     * 数据处理
+     * @param msg 获取的消息
      */
     public void dataProcess(ByteBuf msg){
         //更新帧头的信息
@@ -96,12 +94,12 @@ public class RuiliNodeUdpDataProcessor {
                 .append(RuiliDatadbSegment.MONGODB_KEY_TESTNAME,testName)//测试名称
                 .append(RuiliDatadbSegment.MONGODB_KEY_RAW_DATA,byteRawData );//原始数据
         //解析数据
-        if(dataType.equals(RuiliNodeUdpAttr.ADC_DATA_PACKAGE_STR)) {
+        if(dataType.equals(RCloudNodeAttr.ADC_DATA_PACKAGE_STR)) {
             //生成document
             BasicDBObject bdoAdcVal = getAdcVal4CH(msg,(short)(data_count));
             //解析后的ADC数字量
             doc.append(RuiliDatadbSegment.MONGODB_KEY_ADC_VAL,bdoAdcVal);
-        }else if(dataType.equals(RuiliNodeUdpAttr.CAN_DATA_PACKAGE_STR)) {
+        }else if(dataType.equals(RCloudNodeAttr.CAN_DATA_PACKAGE_STR)) {
             //TODO:(songchaochao,19-4-9,如果是CAN数据则不解析)
         }
 
@@ -118,7 +116,6 @@ public class RuiliNodeUdpDataProcessor {
             logger.error("", e);
         }
     }
-
     /**
      * 数据包的校验方法
      *
@@ -133,7 +130,6 @@ public class RuiliNodeUdpDataProcessor {
             return false;
         }
     }
-
     /**
      * 获取测试名称
      *
@@ -142,65 +138,60 @@ public class RuiliNodeUdpDataProcessor {
      */
     public static String getFrameHeadTestName(ByteBuf msg) {
         //获取测试名称
-        ByteBuf testNameTemp = Unpooled.buffer(RuiliNodeUdpAttr.MAX_TEST_NAME);
-        msg.getBytes(RuiliNodeUdpAttr.TEST_NAME_IDX,testNameTemp,RuiliNodeUdpAttr.MAX_TEST_NAME);
+        ByteBuf testNameTemp = Unpooled.buffer(RCloudNodeAttr.MAX_TEST_NAME);
+        msg.getBytes(RCloudNodeAttr.TEST_NAME_IDX,testNameTemp,RCloudNodeAttr.MAX_TEST_NAME);
         return testNameTemp.toString(CharsetUtil.UTF_8).trim();
     }
+
     /**
-     * 数据包的提取前16bits帧头数据和测试名称
-     *
-     * YYYY_MM_DD:年月日32bits[0:3], HeadTime:毫秒32bits[4:7], count:数据长度32bits[8:11],
-     *
-     * nodeId:模组id8bits[12],IO:数字电平[13],dataType:数据类型[14],checkUbyte:校验8bits[15]
-     *
-     * @param msg
-     * @note msg的两个校验字节msg[4]和msg[15]，相等才能通过
-     * @return true：成功获取数据帧头;false:数据有问题
+     * 获取帧头信息
+     * @param msg 获取的消息
+     * @return 是否获取成功
      */
     private boolean getFrameHead(ByteBuf msg) {
 
         //得到帧头+实际数据的Bytebuf字节长度
         BytebufLength = msg.readableBytes();
         //获取设备的id
-        nodeId = msg.getUnsignedByte(RuiliNodeUdpAttr.WIFI_CLIENT_ID_IDX);
+        nodeId = msg.getUnsignedByte(RCloudNodeAttr.WIFI_CLIENT_ID_IDX);
         //校验设备的id
-        if((nodeId<0) ||(nodeId>RuiliNodeUdpAttr.WIFI_CLIENT_ID_MAX)) {
+        if((nodeId<0) ||(nodeId>RCloudNodeAttr.WIFI_CLIENT_ID_MAX)) {
             logger.warn("NodeId Error : Pkg Abandoned!");
             return false;
         }
         //获取headtime/微秒
-        headtime = (long)(msg.getUnsignedByte(RuiliNodeUdpAttr.HEADTIME_START_IDX)|
-                (msg.getUnsignedByte(RuiliNodeUdpAttr.HEADTIME_START_IDX+1)<<8)|
-                (msg.getUnsignedByte(RuiliNodeUdpAttr.HEADTIME_START_IDX+2)<<16)|
-                (msg.getUnsignedByte(RuiliNodeUdpAttr.HEADTIME_START_IDX+3)<<24));
+        headtime = (long)(msg.getUnsignedByte(RCloudNodeAttr.HEADTIME_START_IDX)|
+                (msg.getUnsignedByte(RCloudNodeAttr.HEADTIME_START_IDX+1)<<8)|
+                (msg.getUnsignedByte(RCloudNodeAttr.HEADTIME_START_IDX+2)<<16)|
+                (msg.getUnsignedByte(RCloudNodeAttr.HEADTIME_START_IDX+3)<<24));
         //获取校验byte
-        checkUbyte = msg.getUnsignedByte(RuiliNodeUdpAttr.CHECK_UBYTE);
+        checkUbyte = msg.getUnsignedByte(RCloudNodeAttr.CHECK_UBYTE);
         //校验位校验，headtime的最低8bits需要和帧头校验位相同
         if(!isRightPkg((short)(headtime&0xff),(short)checkUbyte)){
             logger.warn("CheckUbyte Error : Pkg Abandoned");
             return false;
         }
-        yyyy_mm_dd = (long)(msg.getUnsignedByte(RuiliNodeUdpAttr.YYYY_MM_DD_START_IDX)|
-                (msg.getUnsignedByte(RuiliNodeUdpAttr.YYYY_MM_DD_START_IDX+1)<<8)|
-                (msg.getUnsignedByte(RuiliNodeUdpAttr.YYYY_MM_DD_START_IDX+2)<<16)|
-                (msg.getUnsignedByte(RuiliNodeUdpAttr.YYYY_MM_DD_START_IDX+3)<<24));
-        data_count = (long)(msg.getUnsignedByte(RuiliNodeUdpAttr.DATA_COUNT_START_IDX)|
-                (msg.getUnsignedByte(RuiliNodeUdpAttr.DATA_COUNT_START_IDX+1)<<8)|
-                (msg.getUnsignedByte(RuiliNodeUdpAttr.DATA_COUNT_START_IDX+2)<<16)|
-                (msg.getUnsignedByte(RuiliNodeUdpAttr.DATA_COUNT_START_IDX+3)<<24));
+        yyyy_mm_dd = (long)(msg.getUnsignedByte(RCloudNodeAttr.YYYY_MM_DD_START_IDX)|
+                (msg.getUnsignedByte(RCloudNodeAttr.YYYY_MM_DD_START_IDX+1)<<8)|
+                (msg.getUnsignedByte(RCloudNodeAttr.YYYY_MM_DD_START_IDX+2)<<16)|
+                (msg.getUnsignedByte(RCloudNodeAttr.YYYY_MM_DD_START_IDX+3)<<24));
+        data_count = (long)(msg.getUnsignedByte(RCloudNodeAttr.DATA_COUNT_START_IDX)|
+                (msg.getUnsignedByte(RCloudNodeAttr.DATA_COUNT_START_IDX+1)<<8)|
+                (msg.getUnsignedByte(RCloudNodeAttr.DATA_COUNT_START_IDX+2)<<16)|
+                (msg.getUnsignedByte(RCloudNodeAttr.DATA_COUNT_START_IDX+3)<<24));
         //数据个数的校验
-        if((data_count<0)||(data_count !=(BytebufLength - RuiliNodeUdpAttr.HEAD_FRAME_LENGTH))) {
+        if((data_count<0)||(data_count !=(BytebufLength - RCloudNodeAttr.HEAD_FRAME_LENGTH))) {
             logger.warn("Count Error : Abandoned");
             return false;
         }
         //获取io电平
-        io1 = (short) (msg.getUnsignedByte(RuiliNodeUdpAttr.IO_IN_IDX) &  ((short)0x0001));
-        io2 = (short) ((msg.getUnsignedByte(RuiliNodeUdpAttr.IO_IN_IDX) &  ((short)0x0002))>>1);
+        io1 = (short) (msg.getUnsignedByte(RCloudNodeAttr.IO_IN_IDX) &  ((short)0x0001));
+        io2 = (short) ((msg.getUnsignedByte(RCloudNodeAttr.IO_IN_IDX) &  ((short)0x0002))>>1);
         //获取数据类型，并采取不同的处理措施
-        if(RuiliNodeUdpAttr.CAN_DATA_PACKAGE_LABEL == (short) msg.getUnsignedByte(RuiliNodeUdpAttr.DATA_TYPE_IDX)) {
-            dataType = RuiliNodeUdpAttr.CAN_DATA_PACKAGE_STR;
-        }else if(RuiliNodeUdpAttr.ADC_DATA_PACKAGE_LABEL == (short) msg.getUnsignedByte(RuiliNodeUdpAttr.DATA_TYPE_IDX)) {
-            dataType = RuiliNodeUdpAttr.ADC_DATA_PACKAGE_STR;
+        if(RCloudNodeAttr.CAN_DATA_PACKAGE_LABEL == (short) msg.getUnsignedByte(RCloudNodeAttr.DATA_TYPE_IDX)) {
+            dataType = RCloudNodeAttr.CAN_DATA_PACKAGE_STR;
+        }else if(RCloudNodeAttr.ADC_DATA_PACKAGE_LABEL == (short) msg.getUnsignedByte(RCloudNodeAttr.DATA_TYPE_IDX)) {
+            dataType = RCloudNodeAttr.ADC_DATA_PACKAGE_STR;
         }else {
             logger.warn("Data Type Error : Abandoned");
             return false;
@@ -218,11 +209,9 @@ public class RuiliNodeUdpDataProcessor {
         this.insertIsodate = TimeUtils.getStrIsoSTime();
         return true;
     }
-
     /**
-     * 数据包的提取,除了前16bits帧头外的adc数据，adc数据以[channel1低八位,channel1高八位,channel2低八位,channel2高八位...]传输
-     *
-     * channle_num是通道数，adc_count_short是每个通道包含多少个数据（数据12bit，用hsort）
+     * 数据包的提取,除了前16bits帧头外的adc数据，adc数据以[channel1低八位,channel1高八位,channel2低八位,channel2高八位...]传输<br/>
+     * channle_num是通道数，adc_count_short是每个通道包含多少个数据（数据12bit，用hsort）<br/>
      * ----!!必须在getFrameHead更新后才能调用!!------
      *
      * @param msg 包括帧头在内的所有数据
@@ -230,13 +219,13 @@ public class RuiliNodeUdpDataProcessor {
      * @return BasicDBObject
      */
     private BasicDBObject getAdcVal4CH(ByteBuf msg, int adc_count) {
-        int adc_count_short = adc_count/2/RuiliNodeUdpAttr.ADC_CHANNEL_MAX;
+        int adc_count_short = adc_count/2/RCloudNodeAttr.ADC_CHANNEL_MAX;
         BasicDBList ch1 = new BasicDBList();
         BasicDBList ch2 = new BasicDBList();
         BasicDBList ch3 = new BasicDBList();
         BasicDBList ch4 = new BasicDBList();
         for(int idx = 0,idx_start; idx<adc_count_short ; idx++) {
-            idx_start = idx * 8 + RuiliNodeUdpAttr.HEAD_FRAME_LENGTH;
+            idx_start = idx * 8 + RCloudNodeAttr.HEAD_FRAME_LENGTH;
             ch1.add((short)( (msg.getUnsignedByte(idx_start)<<8) |
                     (msg.getUnsignedByte(idx_start + 1)) ));
             ch2.add((short)( (msg.getUnsignedByte(idx_start + 2)<<8) |
